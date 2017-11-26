4211416670e6bec117b23f2356db5b6c999dea83
Kyler Jordan Cain <kyler@kylercain.com>
2017-11-26 18:56:20 -0500

    Adding stream based import tool

---
+++ Crawler/Lookups/TagNumbers.cs
@@ -0,0 +1,277 @@
+﻿namespace LibraryOfCongressImport.Lookups
+{
+    class TagNumbers
+    {
+        #region Control Fields
+        public const string ControlNumberTag = "001";
+        public const string ControlNumberIdentifierTag = "003";
+        public const string DateAndTimOfLatestTransactionTag = "005";
+        public const string FixedLengthDataElementsOfAdditionalMaterialCharacteristicTag = "006";
+        public const string PhysicalDescriptionFixedFieldTag = "007";
+        public const string FixedLengthDataElementTag = "008";
+        #endregion
+
+        #region Numbers and Codes
+        public const string LibraryofCongressControlNumber = "010";
+        public const string PatentControlInformation = "013";
+        public const string NationalBibliographyNumber = "015";
+        public const string NationalBibliographicAgencyControlNumber = "016";
+        public const string CopyrightorLegalDepositNumber = "017";
+        public const string CopyrightArticleFeeCode = "018";
+        public const string InternationalStandardBookNumber = "020";
+        public const string InternationalStandardSerialNumber = "022";
+        public const string OtherStandardIdentifier = "024";
+        public const string OverseasAcquisitionNumber = "025";
+        public const string FingerprintIdentifier = "026";
+        public const string StandardTechnicalReportNumber = "027";
+        public const string PublisherorDistributorNumber = "028";
+        public const string CODENDesignation = "030";
+        public const string MusicalIncipitsInformation = "031";
+        public const string PostalRegistrationNumber = "032";
+        public const string DateTimeandPlaceofanEvent = "033";
+        public const string CodedCartographicMathematicalData = "034";
+        public const string SystemControlNumber = "035";
+        public const string OriginalStudyNumberforComputerDataFiles = "036";
+        public const string SourceofAcquisition = "037";
+        public const string RecordContentLicensor = "038";
+        public const string CatalogingSource = "040";
+        public const string LanguageCode = "041";
+        public const string AuthenticationCode = "042";
+        public const string GeographicAreaCode = "043";
+        public const string CountryofPublishingProducingEntityCode = "044";
+        public const string TimePeriodofContent = "045";
+        public const string SpecialCodedDates = "046";
+        public const string FormofMusicalCompositionCode = "047";
+        public const string NumberofMusicalInstrumentsorVoicesCodes = "048";
+        public const string LibraryofCongressCallNumber = "050";
+        public const string LibraryofCongressCopyIssueOrOffprintStatement = "051";
+        public const string GeographicClassification = "052";
+        public const string ClassificationNumbersAssignedinCanada = "055";
+        public const string NationalLibraryofMedicineCallNumber = "060";
+        public const string NationalLibraryofMedicineCopyStatement = "061";
+        public const string CharacterSetsPresent = "066";
+        public const string NationalAgriculturalLibraryCallNumber = "070";
+        public const string NationalAgriculturalLibraryCopyStatement = "071";
+        public const string SubjectCategoryCode = "072";
+        public const string GPOItemNumber = "074";
+        public const string UniversalDecimalClassificationNumber = "080";
+        public const string DeweyDecimalClassificationNumber = "082";
+        public const string AdditionalDeweyDecimalClassificationNumber = "083";
+        public const string OtherClassificationNumber = "084";
+        public const string SynthesizedClassificationNumberComponents = "085";
+        public const string GovernmentDocumentClassificationNumber = "086";
+        public const string ReportNumber = "088";
+        #endregion
+
+        #region Main Entries
+        public const string PersonalNameMainEntry = "100";
+        public const string CorporateNameMainEntry = "110";
+        public const string MeetingNameMainEntry = "111";
+        public const string UniformTitleMainEntry = "130";
+        #endregion
+
+        #region Title and Title Related Fields
+        public const string AbbreviatedTitle = "210";
+        public const string KeyTitle = "222";
+        public const string UniformTitle = "240";
+        public const string TranslationOfTitleByCatalogingAgency = "242";
+        public const string CollectiveUniformTitle = "243";
+        public const string TitleStatement = "245";
+        public const string VaryingFormOfTitle = "246";
+        public const string FormerTitle = "247";
+        #endregion
+
+        #region Edition Imprint and other Fields
+        public const string EditionStatement = "250";
+        public const string MusicalPresentationStatement = "254";
+        public const string CartographicMathematicalData = "255";
+        public const string ComputerFileCharacteristics = "256";
+        public const string CountryOfProducingEntity = "257";
+        public const string PhilatelicIssueData = "258";
+        public const string PublicationDistribution = "260";
+        public const string ProjectedPublicationDate = "263";
+        public const string ProductionPublicationDistributionManufactureAndCopyrightNotice = "264";
+        public const string Address = "270";
+        #endregion
+
+        #region Physical Description
+        public const string PhysicalDescription = "300";
+        public const string PlayingTime = "306";
+        public const string HoursEtc = "307";
+        public const string CurrentPublicationFrequency = "310";
+        public const string FormerPublicationFrequency = "321";
+        public const string ContentType = "336";
+        public const string MediaType = "337";
+        public const string CarrierType = "338";
+        public const string PhysicalMedium = "340";
+        public const string GeospatialReferenceData = "342";
+        public const string PlanarCoordinateData = "343";
+        public const string SoundCharacteristics = "344";
+        public const string ProjectionCharacteristicsOfMovingImage = "345";
+        public const string VideoCharacteristics = "346";
+        public const string DigitalFileCharacteristics = "347";
+        public const string FormatOfNotatedMusic = "348";
+        public const string OrganizationAndArrangementOfMaterials = "351";
+        public const string DigitalGraphicRepresentation = "352";
+        public const string SecurityClassificationControl = "355";
+        public const string OriginatorDisseminationControl = "357";
+        public const string DatesOfPublicationAndSequentialDesignation = "362";
+        public const string NormalizedDateAndSequentialDesignation = "363";
+        public const string TradePrice = "365";
+        public const string TradeAvailabilityInformation = "366";
+        public const string AssociatedPlace = "370";
+        public const string AssociatedLanguage = "377";
+        public const string FormOfWork = "380";
+        public const string OtherDistinguishingCharacteristicsOfWorkOrExpression = "381";
+        public const string MediumOfPerformance = "382";
+        public const string NumericDesignationOfMusicalWork = "383";
+        public const string Key = "384";
+        public const string AudienceCharacteristics = "385";
+        public const string CreatorcontributorCharacteristics = "386";
+        public const string TimePeriodOfCreation = "388";
+        #endregion
+
+        #region Series Statement
+        public const string SeriesStatement = "490";
+        #endregion
+
+        #region Note Fields
+        public const string GeneralNote = "500";
+        public const string WithNote = "501";
+        public const string DissertationNote = "502";
+        public const string BibliographyNote = "504";
+        public const string FormattedContentsNote = "505";
+        public const string RestrictionsOnAccessNote = "506";
+        public const string ScaleNoteForGraphicMaterial = "507";
+        public const string CreationproductionCreditsNote = "508";
+        public const string CitationreferencesNote = "510";
+        public const string ParticipantOrPerformerNote = "511";
+        public const string TypeOfReportAndPeriodCoveredNote = "513";
+        public const string DataQualityNote = "514";
+        public const string NumberingPeculiaritiesNote = "515";
+        public const string TypeOfComputerFileOrDataNote = "516";
+        public const string DatetimeAndPlaceOfAnEventNote = "518";
+        public const string Summary = "520";
+        public const string TargetAudienceNote = "521";
+        public const string GeographicCoverageNote = "522";
+        public const string PreferredCitationOfDescribedMaterialsNote = "524";
+        public const string SupplementNote = "525";
+        public const string StudyProgramInformationNote = "526";
+        public const string AdditionalPhysicalFormAvailableNote = "530";
+        public const string ReproductionNote = "533";
+        public const string OriginalVersionNote = "534";
+        public const string LocationOfOriginalsduplicatesNote = "535";
+        public const string FundingInformationNote = "536";
+        public const string SystemDetailsNote = "538";
+        public const string TermsGoverningUseAndReproductionNote = "540";
+        public const string ImmediateSourceOfAcquisitionNote = "541";
+        public const string InformationRelatingToCopyrightStatus = "542";
+        public const string LocationOfOtherArchivalMaterialsNote = "544";
+        public const string BiographicalOrHistoricalData = "545";
+        public const string LanguageNote = "546";
+        public const string FormerTitleComplexityNote = "547";
+        public const string IssuingBodyNote = "550";
+        public const string EntityAndAttributeInformationNote = "552";
+        public const string CumulativeIndexfindingAidsNote = "555";
+        public const string InformationAboutDocumentationNote = "556";
+        public const string OwnershipAndCustodialHistory = "561";
+        public const string CopyAndVersionIdentificationNote = "562";
+        public const string BindingInformation = "563";
+        public const string CaseFileCharacteristicsNote = "565";
+        public const string MethodologyNote = "567";
+        public const string LinkingEntryComplexityNote = "580";
+        public const string PublicationsAboutDescribedMaterialsNote = "581";
+        public const string ActionNote = "583";
+        public const string AccumulationAndFrequencyOfUseNote = "584";
+        public const string ExhibitionsNote = "585";
+        public const string AwardsNote = "586";
+        public const string SourceOfDescriptionNote = "588";
+        #endregion
+
+        #region Subject Access Fields
+        public const string SubjectAddedEntryPersonalName = "600";
+        public const string SubjectAddedEntryCorporateName = "610";
+        public const string SubjectAddedEntryMeetingName = "611";
+        public const string SubjectAddedEntryUniformTitle = "630";
+        public const string SubjectAddedEntryNamedEvent = "647";
+        public const string SubjectAddedEntryChronologicalTerm = "648";
+        public const string SubjectAddedEntryTopicalTerm = "650";
+        public const string SubjectAddedEntryGeographicName = "651";
+        public const string IndexTermUncontrolled = "653";
+        public const string SubjectAddedEntryFacetedTopicalTerms = "654";
+        public const string IndexTermGenreForm = "655";
+        public const string IndexTermOccupation = "656";
+        public const string IndexTermFunction = "657";
+        public const string IndexTermCurriculumObjective = "658";
+        public const string SubjectAddedEntryHierarchicalPlaceName = "662";
+        #endregion
+
+        #region Added Entry Fields
+        public const string AddedEntryPersonalName = "700";
+        public const string AddedEntryCorporateName = "710";
+        public const string AddedEntryMeetingName = "711";
+        public const string AddedEntryUncontrolledName = "720";
+        public const string AddedEntryUniformTitle = "730";
+        public const string AddedEntryUncontrolledRelatedAnalyticalTitle = "740";
+        public const string AddedEntryGeographicName = "751";
+        public const string AddedEntryHierarchicalPlaceName = "752";
+        public const string SystemDetailsAccesstoComputerFiles = "753";
+        public const string AddedEntryTaxonomicIdentification = "754";
+        #endregion
+
+        #region Linking Entry Fields
+        public const string MainSeriesEntry = "760";
+        public const string SubseriesEntry = "762";
+        public const string OriginalLanguageEntry = "765";
+        public const string TranslationEntry = "767";
+        public const string SupplementSpecialIssueEntry = "770";
+        public const string SupplementParentEntry = "772";
+        public const string HostItemEntry = "773";
+        public const string ConstituentUnitEntry = "774";
+        public const string OtherEditionEntry = "775";
+        public const string AdditionalPhysicalFormEntry = "776";
+        public const string IssuedWithEntry = "777";
+        public const string PrecedingEntry = "780";
+        public const string SucceedingEntry = "785";
+        public const string DataSourceEntry = "786";
+        public const string OtherRelationshipEntry = "787";
+        #endregion
+
+        #region Series Added Entry Fields
+        public const string SeriesAddedEntryPersonalName = "800";
+        public const string SeriesAddedEntryCorporateName = "810";
+        public const string SeriesAddedEntryMeetingName = "811";
+        public const string SeriesAddedEntryUniformTitle = "830";
+        #endregion
+
+        #region Holdings and Alternate Graphics
+        public const string HoldingsCodedDataValues = "841";
+        public const string TextualPhysicalFormDesignator = "842";
+        public const string HoldingsReproductionNote = "843";
+        public const string NameofUnit = "844";
+        public const string TermsGoverningUseandReproduction = "845";
+        public const string HoldingInstitution = "850";
+        public const string Location = "852";
+        public const string CaptionsandPatternBasicBibliographicUnit = "853";
+        public const string CaptionsandPatternSupplementaryMaterial = "854";
+        public const string CaptionsandPatternIndexes = "855";
+        public const string ElectronicLocationandAccess = "856";
+        public const string EnumerationandChronologyBasicBibliographicUnit = "863";
+        public const string EnumerationandChronologySupplementaryMaterial = "864";
+        public const string EnumerationandChronologyIndexes = "865";
+        public const string TextualHoldingsBasicBibliographicUnit = "866";
+        public const string TextualHoldingsSupplementaryMaterial = "867";
+        public const string TextualHoldingsIndexes = "868";
+        public const string ItemInformationBasicBibliographicUnit = "876";
+        public const string ItemInformationSupplementaryMaterial = "877";
+        public const string ItemInformationIndexes = "878";
+        public const string AlternateGraphicRepresentation = "880";
+        public const string ReplacementRecordInformation = "882";
+        public const string MachinegeneratedMetadataProvenance = "883";
+        public const string DescriptionConversionInformation = "884";
+        public const string MatchingInformation = "885";
+        public const string ForeignMARCInformationField = "886";
+        public const string NonMARCInformationField = "887";
+        #endregion
+    }
+}
---
+++ Crawler/Tools/DBTools.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryOfCongressImport.Tools
+{
+    public static class DBTools
+    {
+        public static void PushItemToDatabase(ref Item item)
+        {
+            // create/locate item based on some identifying feature
+            // get id
+        }
+
+        private static void PushItemAttributesToDatabase(ref Item item)
+        {
+            foreach (var attribute in item.Attributes)
+            {
+                // create/locate new attributes in db as they appear
+                // get attribute id
+                // cache
+                // create/locate attribute value for item
+                // cache
+                // connect the two in db
+            }
+        }
+    }
+}
