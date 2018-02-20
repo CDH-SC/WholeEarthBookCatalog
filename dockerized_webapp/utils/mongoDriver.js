9218a8c1db697ab83a71f1d00c8d8e1e510c4039
jacksonarnold <jacksonarnold96@gmail.com>
2018-02-20 16:53:54 -0500

    Moved clock to seperate file
    Moved clock from mongoDriver to seperate clock file

--- dockerized_webapp/utils/mongoDriver.js
+++ dockerized_webapp/utils/mongoDriver.js
@@ -6,7 +6,7 @@
 
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var moment = require('moment');
+// var moment = require('moment');
 var url = `mongodb://${process.env.MONGO_URL}`;
 
 // module
@@ -55,51 +55,4 @@ driver.updateDocument = function(doc, updatedoc, callback) {
 }
 
 
-<<<<<<< Updated upstream
-// Values for clock to monitor number of searches
-var midnight = "23:59:59";
-var deductible;
-const searchAmount = 50000;
-
-// Clock object
-function My_Clock() {
-    this.currTime = moment().format("HH:mm:ss");
-}
-
-// updates clock every second
-My_Clock.prototype.run = function() {
-    setInterval(this.update.bind(this), 1000);
-}
-
-// Clock update function
-My_Clock.prototype.update = function() {
-
-    this.currTime = moment().format("HH:mm:ss");
-    console.log(moment().format("HH:mm:ss"));
-    // var testMidnight = moment().endOf('day').format("HH:mm:ss");
-
-    // Reset deductible at midnight
-    if (midnight === this.currTime) {
-        this.resetDeductible();
-    }
-}
-
-// Resetting deductible to 50,000 searches
-My_Clock.prototype.resetDeductible = function() {
-    console.log("Reset deductible");
-    deductible = 50000;
-}
-
-// Set deductible and run clock
-driver.setUpClock = function() {
-    deductible = 50000;
-    console.log("Setting up clock");
-    var clock = new My_Clock();
-    clock.run();
-}
-
-
 module.exports = driver;
-=======
-module.exports = driver;
->>>>>>> Stashed changes
