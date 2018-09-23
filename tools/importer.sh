1042add8db23def6f114667b1037c2fb2522ce1c
clayscode <hcnorris@email.sc.edu>
2018-09-22 21:13:56 -0400

    Add headers, fix importer.sh, and add all functionality to locextractor.py

--- tools/importer.sh
+++ tools/importer.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 if [ $# -eq 3 ];then
-    neo4j-admin import --mode=csv --database "$1" --id-type string \ 
-                        --nodes:person headers/person_header.csv,data/person_batch.csv \ 
-                        --nodes:edition headers/edition_header.csv,data/edition_batch.csv \ 
-                        --nodes:publisher headers/publisher_header.csv,data/publisher_batch.csv \ 
-                        --nodes:place headers/place_header.csv,data/place_batch.csv \ 
-                        --relationships:WROTE headers/wrote_rel_header.csv,data/wrote_rel_batch.csv \ 
-                        --relationships:PUBLISHED headers/published_rel_header.csv,data/published_rel_batch.csv \ 
-                        --relationships:PUBLISHED_IN headers/place_rel_header.csv,data/place_rel_batch.csv \ 
-                        --relationships:PUBLISHED_IN headers/edition_rel_header.csv,data/edition_rel_batch.csv  \ 
+    neo4j-admin import --mode=csv --database "$1" --id-type string \
+                        --nodes "headers/person_header.csv,data/person_batch.csv" \
+                        --nodes "headers/edition_header.csv,data/edition_batch.csv" \
+                        --nodes "headers/publisher_header.csv,data/publisher_batch.csv" \
+                        --nodes "headers/place_header.csv,data/place_batch.csv" \
+                        --relationships "headers/wrote_rel_header.csv,data/wrote_rel_batch.csv" \
+                        --relationships "headers/published_rel_header.csv,data/published_rel_batch.csv" \
+                        --relationships "headers/place_rel_header.csv,data/place_rel_batch.csv" \
                         --ignore-duplicate-nodes true --delimiter "$2" --high-io true --quote "$3"
 
 else
--- tools/locextractor.py
+++ tools/locextractor.py
@@ -21,6 +21,13 @@ class LocExtractor():
             for line in data:
                 csv_writer.writerow(line)
 
+    def csvReader(self, fname, delimiter, quotechar):
+        with open(str(fname), "r", newline='\n') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=delimiter, quotechar=quotechar)
+            for row in csv_reader:
+                yield row    
+        
+
     def nodeExtractor(self,data,nodes,key):
         keys = nodes[key]['keys']
         id_nodes = ['editionISBN','person','place','publisher']
@@ -49,16 +56,23 @@ class LocExtractor():
                         extracted.append(self.current_ID)
                         self.current_ID += 1
                     extracted.append("unknown")
+            extracted.append(key)
             yield extracted
              
-    #TODO relationship extractor
-    def relExtractor(self,data,nodes,label):
-        return 
-    
-    
+    def relExtractor(self,keys,label,nodes, delimiter, quotechar):
+        node1 = self.csvReader(fname=nodes[keys[0]]['fname'],
+                                delimiter=delimiter,
+                                quotechar=quotechar)
+        
+        node2 = self.csvReader(fname=nodes[keys[1]]['fname'],
+                                delimiter=delimiter,
+                                quotechar=quotechar)
+
+        for n1,n2 in zip(node1,node2):
+            yield [n1[0],n2[0],label] 
 
 
-def main(key):
+def extractNode(key):
     extractor = LocExtractor()
     if args.all:
         batches = ["batch{}.json".format(x) for x in range(0,118)]
@@ -67,29 +81,32 @@ def main(key):
     
     # Load each batch individually. Takes a lot less ram. 
     for batch in batches:
-        with open(batch,'r') as f:
+        batch = Path(args.b) / batch
+        with open(str(batch),'r') as f:
             data = json.load(f)
             extractor.csvWriter(extractor.nodeExtractor(data=data,nodes=nodes,key=key),fname=nodes[key]['fname'],delimiter=args.d,quotechar=args.q)  
 
 
-#    for key in relations.keys():
-#        csv_writer(relExtractor(nodes=relations[key]['nodes'],
-#                   label=relations[key]['label']),
-#                   fname=relations[key]['fname'],delimiter=args.d,quotes=args.q)
-#
+def extractRel(key):
+    extractor = LocExtractor()
+    extractor.csvWriter(extractor.relExtractor(keys=relations[key]['nodes'],
+               label=relations[key]['label'],nodes=nodes,delimiter=args.d,quotechar=args.q),
+               fname=relations[key]['fname'],delimiter=args.d,quotechar=args.q)
+
                  
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='locextract.py', description="Converts LOC dataset into CSV that is parsable by Neo4j", usage="./locextract -p /some/file/path -q quotechar -d delimiter -s startbatch -f endbatch")
+    parser = argparse.ArgumentParser(prog='locextract.py', description="Converts LOC dataset into CSV that is parsable by Neo4j", usage="./locextract -b /batch/file/path -e /extracted/file/path -q quotechar -d delimiter -s startbatch -f endbatch")
     parser.add_argument("--all", help='Process all batches in current directory')
-    parser.add_argument("-p",default='/neo4j/data/loc/extracted/data/',help='Specify File Path')
+    parser.add_argument("-e",default='/neo4j/data/loc/extracted/data/',help='Specify extraction path')
+    parser.add_argument("-b", default='/neo4j/data/loc', help="Specify batch location")
     parser.add_argument("-q",default='"', help='Specify quotation character')
     parser.add_argument("-d", default="|", help='Specifiy delimiter')
     parser.add_argument("-s", default=0, help='Specify start batch')
     parser.add_argument("-f", default=0, help='Specify finish batch')
     args = parser.parse_args()
 
-    save_dir = Path(args.p)
+    save_dir = Path(args.e)
 
     #Define all possible node types. fname is the final save directory of the extracted batch
     nodes = { 'Place': {
@@ -130,7 +147,16 @@ if __name__ == "__main__":
             }  
 
     #Spawn a new process for every key for an easy 4X performance boost
-    processes = [Process(target=main, args=(key,)) for key in nodes.keys()]
+    processes = [Process(target=extractNode, args=(key,)) for key in nodes.keys()]
     for p in processes:
         p.start()
-        
+    for p in processes:
+        p.join() 
+
+    #Could probably get away with staggering instead of joining processes above
+    #but best to avoid race conditions
+    processes = [Process(target=extractRel, args=(key,)) for key in relations.keys()]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
