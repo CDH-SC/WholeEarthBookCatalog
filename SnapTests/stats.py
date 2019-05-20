d2748eb4f62dcffe807971ee77dd444e635c1199
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2019-05-20 15:24:13 -0400

    Delete stats.py
    already in repo

--- SnapTests/stats.py
+++
@@ -1,7 +0,0 @@
-import snap
-# This loads the graph in from the saved import that was conducted in dhc_dataimp.py
-filename = "graph/dhc_graph"
-FIn = snap.TFIn(filename)
-g = snap.TNGraph.Load(FIn)
-
-snap.PrintInfo(g, "Python type PNGraph", "dhcinfoprint.txt", False)
