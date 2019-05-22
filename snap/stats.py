1aaf33f51c8ba82a50aa43da5d5c0521475ffe39
samyuc <33384568+samyuc@users.noreply.github.com>
2019-05-22 11:11:08 -0400

    Update stats.py

--- snap/stats.py
+++ snap/stats.py
@@ -1,5 +1,5 @@
 import snap
-# This loads the graph in from the saved import that was conducted in dhc_dataimp.py
+# This loads the graph in from the saved import that was conducted in dataimport.py
 filename = "graph/dhc_graph"
 FIn = snap.TFIn(filename)
 g = snap.TNGraph.Load(FIn)
