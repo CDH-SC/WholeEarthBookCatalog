cf03e61677372a6704662622bb0d3d7dd37b38ae
clayscode <hcnorris@email.sc.edu>
2019-10-02 09:16:56 -0400

    Add more useful utils

--- scripts_to_format_viaf_data/counter.sh
+++ scripts_to_format_viaf_data/counter.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-vals=( coAuthor pub titles countries)
+vals=( coAuthor pub titles countries isbns)
 for i in "${vals[@]}"
 do
 	echo "----${i} Count----" >> counts
 	cut -f2 ${i}Table.tsv | sort | uniq | wc -l >> counts
 done
-vals2=( aliases isbns person )
+vals2=( aliases )
 for i in "${vals2[@]}"
 do
 	echo "----${i} Count----" >> counts
-	cut -f1 ${i}Table.tsv | sort | uniq | wc -l >> counts
+	cut -f1 ${i}Table.tsv | sort | wc -l >> counts
 done
+
+echo "----person Count----" >> counts
+cut -f1 personTable.tsv | sort | uniq | wc -l >> counts
